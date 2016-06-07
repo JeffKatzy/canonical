56c664097273e3b2d299497a080f1a88512c8f5f
Jeff Katz <jeffreyerickatz@gmail.com>
2016-06-07 10:00:13 -0400

    refactoring gosu library

---
+++ gosu/song.rb
@@ -0,0 +1,23 @@
+require 'bundler/setup'
+Bundler.require
+require 'pry'
+load_all './lib'
+
+ play('lib/notes/D1.wav', 1)
+  # play('notes/E1.wav', 1)
+  # play('notes/F1.wav', 1)
+  # play('notes/G1.wav', 1)
+
+
+
+ 
+
+ # 1. Play individual note
+      # d triggers d note
+ # 2. Iterate through this process
+        # move through each element of the string
+
+
+
+
+
